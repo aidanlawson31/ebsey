804093523630eb06b70342410887f3f6060d0fd8
AidanL <aidanlawson31@gmail.com>
2019-07-03 15:43:06 +1000

    minor model fix

--- spec/models/categories_spec.rb
+++ spec/models/categories_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Category, type: :model do
   subject { create(:category) }
 
-  describe 'Validations' do
+  describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name) }
 
